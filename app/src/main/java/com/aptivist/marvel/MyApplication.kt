61a9fe05eebd11ee790c2a0b43a1a712efcea5d5
Diego Garcia <diegogaral1996@outlook.com>
2023-01-16 13:13:15 -0500

    Remove Hilt annotation and add Koin syntax

--- app/src/main/java/com/aptivist/marvel/MyApplication.kt
+++ app/src/main/java/com/aptivist/marvel/MyApplication.kt
@@ -1,9 +1,24 @@
 package com.aptivist.marvel
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
+import com.aptivist.marvel.core.di.appModule
+import com.aptivist.marvel.core.di.networkModule
+import com.aptivist.marvel.core.di.repositoryModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
-
-@HiltAndroidApp
 class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin{
+            androidLogger(Level.DEBUG)
+            androidContext(this@MyApplication)
+            modules(
+                appModule,repositoryModule, networkModule
+            )
+        }
+    }
 }
